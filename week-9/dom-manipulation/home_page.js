0b498ffb3d379096704807bf1924bb2e34db46a7
Buck Melton <buck9063@gmail.com>
2016-04-10 03:50:01 +0000

    Move reflection to html file

--- week-9/dom-manipulation/home_page.js
+++ week-9/dom-manipulation/home_page.js
@@ -39,23 +39,3 @@ for (var i=0; i<rel4elements.length; i++) {
 var tmpl = document.getElementById('hidden');
 document.body.appendChild(tmpl.content.cloneNode(true));
 
-/*
-
-- What did you learn about the DOM?
-
-	I learned that the DOM is a big tree that includes nodes and leaves for all the elements on a web page.
-	Using built-in JavaScript methods, you can access all of the nodes and leaves and manipulate and change
-	them dynamically, altering a web page after it's been displayed.
-
-- What are some useful methods to use to manipulate the DOM?
-
-	document.getElementById - get the element on the page with the given id.
-	document.getElementsByTagName - get an array of all elements that have a given tag.
-	Node.childNodes - get an array of all child elements of the node.
-	Node.firstChild - get the first child of the node.
-	Node.lastChild - get the last child of the node.
-	Node.parent - get the parent of the node.
-	Node.nextSibling - get the next child of your parent node
-	Node.previousSibling - get the previous child of your parent node
-
-*/
